a48777295d0323752c95654654b13d965dc437c6
Vincent <asdll1234567@gmail.com>
2021-05-03 00:39:05 +0800

    修改後臺新增資料&獲取資料的方式

--- app/src/main/java/ManagerPage/ManagerActivity.kt
+++ app/src/main/java/ManagerPage/ManagerActivity.kt
@@ -9,19 +9,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.FileProvider
 import com.bumptech.glide.Glide
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
 import com.vincent.ebook.R
 import kotlinx.android.synthetic.main.activity_manager.*
 import kotlinx.android.synthetic.main.view_upload_book.*
-import utils.Book
+import utils.FireBaseUtils
 import java.io.File
 
 class ManagerActivity : AppCompatActivity() {
@@ -30,8 +25,6 @@ class ManagerActivity : AppCompatActivity() {
     private val managerActivity = this
     private lateinit var uploadBookImageFile : File
     private lateinit var uploadBookImageUri : Uri
-    private val firestore = FirebaseFirestore.getInstance()
-    private val storageRef = FirebaseStorage.getInstance().reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,10 +36,36 @@ class ManagerActivity : AppCompatActivity() {
     private fun initButton(){
 
         manager_button_submit.setOnClickListener {
-            uploadBookImage()
+            val name = manager_edit_name.text.toString()
+            val author = manager_edit_author.text.toString()
+            val version = manager_edit_version.text.toString()
+            val publishData = manager_edit_publish_date.text.toString()
+            val publisher = manager_edit_publisher.text.toString()
+            val size = manager_edit_size.text.toString()
+            val isbn = manager_edit_ISBN.text.toString()
+            val translator = manager_edit_translator.text.toString()
+            val relatedLink = manager_edit_related_link.text.toString()
+
+            if (!(name.isNullOrBlank()
+                        || author.isNullOrBlank()
+                        || version.isNullOrBlank()
+                        || publishData.isNullOrBlank()
+                        || publisher.isNullOrBlank()
+                        || size.isNullOrBlank()
+                        || isbn.isNullOrBlank()
+                        || translator.isNullOrBlank()
+                        || relatedLink.isNullOrBlank())
+            ) {
+                FireBaseUtils.setUploadInfoAndUpload(uploadBookImageUri.lastPathSegment ?: "",uploadBookImageUri, name, author,
+                version,publishData,publisher,size,isbn,translator,relatedLink)
+                Log.d("TAG", "@@@@ after upload book info ")
+            }
         }
         manager_button_cancel.setOnClickListener {
-
+            for(book in FireBaseUtils.getBookInfo()){
+                Log.d("TAG", "@@@@ retrieved ${book.name} \n ${book.author} \n ${book.version} \n ${book.publishDate} \n ${book.publisher}\n" +
+                        "${book.size} \n ${book.isbn} \n ${book.translator} \n ${book.relatedLink} \n ${book.uri} \n @@@@")
+            }
         }
     }
 
@@ -55,95 +74,13 @@ class ManagerActivity : AppCompatActivity() {
      *  先取出各個 edittext 的 value，再判斷是否為空，若不為空的話就可以上傳
      */
 
-    private fun uploadBookInfo(downloadUri : String) {
-
-        val name = manager_edit_name.text.toString()
-        val author = manager_edit_author.text.toString()
-        val version = manager_edit_version.text.toString()
-        val publishData = manager_edit_publish_date.text.toString()
-        val publisher = manager_edit_publisher.text.toString()
-        val size = manager_edit_size.text.toString()
-        val isbn = manager_edit_ISBN.text.toString()
-        val translator = manager_edit_translator.text.toString()
-        val relatedLink = manager_edit_related_link.text.toString()
-
-        if(!(name.isNullOrBlank()
-            && author.isNullOrBlank()
-            && version.isNullOrBlank()
-            && publishData.isNullOrBlank()
-            && publisher.isNullOrBlank()
-            && size.isNullOrBlank()
-            && isbn.isNullOrBlank()
-            && translator.isNullOrBlank()
-            && relatedLink.isNullOrBlank())){
-
-            val book = Book(name,
-                author,
-                version.toInt(),
-                publishData.toInt(),
-                publisher,
-                size,
-                isbn,
-                translator,
-                relatedLink,
-                downloadUri)
-
-            firestore.collection("BooksCollection").document("Book2").set(book)
-                .addOnSuccessListener {
-                    Toast.makeText(this,"upload success",Toast.LENGTH_SHORT).show()
-                    Log.d("TAG", "@@@@ upload info success")
-                }
-                .addOnFailureListener {
-                    Toast.makeText(this,"upload Fail",Toast.LENGTH_SHORT).show()
-                    Log.d("TAG", "@@@@ upload info fail")
-                }
-        }
-    }
-
-    /**
-     *  此方法在成功上傳圖片至 firebase 後會呼叫 uploadBookInfo
-     */
-
-    private fun uploadBookImage(){
-
-        val riversRef = storageRef.child(uploadBookImageUri.lastPathSegment ?: "")
-        riversRef.putFile(uploadBookImageUri).addOnSuccessListener {
-            it.storage.downloadUrl.addOnSuccessListener {
-                uploadBookInfo(it.toString())
-            }
-            manager_upload_progress.visibility = View.GONE
-        }.addOnFailureListener {
-
-        }.addOnProgressListener {
-            val progress = (100.0 * it.bytesTransferred / it.totalByteCount).toInt()
-            if(manager_upload_progress.visibility == View.GONE){
-                manager_upload_progress.visibility = View.VISIBLE
-                manager_upload_progress.progress = progress
-                if(progress >= 100){
-                    manager_upload_progress.visibility = View.GONE
-                }
-            }
-            // 按下上傳後會自動將畫面滾動至最上方，方便查看上傳的進度
-            manager_nested_scroll_view.fullScroll(View.FOCUS_UP)
-            manager_nested_scroll_view.scrollTo(0,0)
-        }
-    }
-
-    private fun downloadImage(ref : StorageReference){
-        ref.downloadUrl
-            .addOnSuccessListener {
-                Log.d("TAG", "@@@@ download url =  ${it.toString()}")
-            }.addOnFailureListener {
-                Log.d("TAG", "@@@@ exception = ${it.printStackTrace()}")
-            }
-    }
 
     private fun checkPermission(){
         val permission = ActivityCompat.checkSelfPermission(this,android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
         if(permission != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),100)
         }else{
-            uploadBookImage()
+            //uploadBookImage()
         }
     }
 
@@ -152,7 +89,7 @@ class ManagerActivity : AppCompatActivity() {
         when(requestCode){
             100 -> {
                 if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    uploadBookImage()
+                    //uploadBookImage()
                 }else{
 
                 }
