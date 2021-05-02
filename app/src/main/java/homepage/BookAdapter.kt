e37a5db204614fd54673b1c4e3cc00a41ce7556c
Vincent <asdll1234567@gmail.com>
2021-05-02 23:01:10 +0800

    調整 viewHolder 位置及設定 view 相關的程式碼

--- app/src/main/java/homepage/BookAdapter.kt
+++ app/src/main/java/homepage/BookAdapter.kt
@@ -1,6 +1,7 @@
 package Homepage
 
 import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,10 +14,6 @@ import com.vincent.ebook.R
 import utils.Book
 
 class BookAdapter (val context : Context, val bookList : List<Book>) : RecyclerView.Adapter<BookAdapter.ViewHolder>() {
-    inner class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
-        val bookImage = view.findViewById<ImageView>(R.id.recycler_item_book_image)
-        val bookName = view.findViewById<TextView>(R.id.recycler_item_book_name)
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_item_book,parent,false)
@@ -31,8 +28,16 @@ class BookAdapter (val context : Context, val bookList : List<Book>) : RecyclerV
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val book = bookList[position]
-        //Glide.with(context).load(book.bookImageId).into(holder.bookImage)
-        holder.bookName.text = book.name
+        holder.setupView(bookList[position])
+    }
+
+    inner class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
+        val bookImage = view.findViewById<ImageView>(R.id.recycler_item_book_image)
+        val bookName = view.findViewById<TextView>(R.id.recycler_item_book_name)
+
+        fun setupView(book : Book){
+            Glide.with(context).load(Uri.parse(book.uri)).into(bookImage)
+            bookName.text = book.name
+        }
     }
 }
