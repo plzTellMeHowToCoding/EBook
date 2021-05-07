05aae56a12c8c00531526123fabaaee2e1d67aa4
Vincent <asdll1234567@gmail.com>
2021-05-08 00:54:57 +0800

    新增一鍵滑到頁面頂端按鈕、移除 ViewPager Layout、修改 Floating Action Button 滑動效果

--- app/src/main/java/homepage/ViewPagerContentAdapter.kt
+++ app/src/main/java/homepage/ViewPagerContentAdapter.kt
@@ -1,28 +1,19 @@
 package Homepage
 
+import android.content.Context
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import com.vincent.ebook.R
 
 
 class ViewPagerContentAdapter(private val titleList: Array<String>, fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
-
     override fun getItem(position: Int): Fragment {
-        return when (position) {
-            0 -> {
-                Log.d("TAG", "@@@@ in 0 page ")
-                BookFragment()
-            }
-            1 -> {
-                Log.d("TAG", "@@@@ in 1 page ")
-                MagazineFragment()
-            }
-            // IDE 提醒要 + else branch，這邊隨便回傳一個 fragment
-            else -> {
-                BookFragment()
-            }
-        }
+        return BookFragment()
     }
 
     override fun getCount(): Int {
@@ -30,7 +21,13 @@ class ViewPagerContentAdapter(private val titleList: Array<String>, fm: Fragment
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
+        Log.d("TAG", "@@@@ init tab  ")
         return if(titleList.isNotEmpty()) titleList[position] else ""
     }
 
+    fun getTabView(context : Context, position : Int) : View {
+        val view = LayoutInflater.from(context).inflate(R.layout.home_tab_category,null)
+        view.findViewById<TextView>(R.id.home_tab_category_tv).text = titleList[position]
+        return view
+    }
 }
